e1f532ac95305a9a9fc879c1e8ede069f35acaa3
Dynamicphavy <favourajayi308@gmail.com>
2025-09-28 19:05:28 +0100

    reset

---
+++ src/pages/reset/Reset.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import SignCard from ''
+import ResetForm from './ResetForm'
+
+const Reset = () => {
+  return (
+    <div>
+      <div>
+        <SignCard />
+        <ResetForm />
+      </div>
+    </div>
+  )
+}
+
+export default Reset;
